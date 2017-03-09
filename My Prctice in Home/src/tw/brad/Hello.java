e3c4611073e80e1fc42e78ec6d3346922542bb17
承哲 <@qoo85714>
2017-03-09 20:00:25 +0800

    20170309

---
+++ My Prctice in Home/src/tw/brad/Hello.java
@@ -0,0 +1,12 @@
+package tw.brad;
+
+public class Hello {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello,World");
+		System.out.println("Hello,Jason");
+
+	}
+
+}
---
+++ My Prctice in Home/src/tw/brad/Jason01.java
@@ -0,0 +1,10 @@
+package tw.brad;
+
+public class Jason01 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
